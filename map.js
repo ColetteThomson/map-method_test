67367f56239936b2b57d920beba0051c5cbfed7c
ColetteThomson <colette_f_thomson@yahoo.co.uk>
2021-11-02 12:52:40 +0000

    add examples

--- map.js
+++ map.js
@@ -5,15 +5,33 @@
  */
 
 // Using a for loop
+let nums = [1, 2, 3, 4, 5];
+let results = [];
+for (let num of nums) {
+  results.push(num * 2);
+}
+console.log(results);
 
 
 // Using map()
+const multByTwo = function (nums) {
+  return nums * 2;
+}
+
+const mapResults = nums.map(multByTwo);
+console.log(mapResults);
 
 
-// Simplified w/ map()
+// Simplified w/ map() - 
+// above function is moved inside the parentheses. Notice it  doesn’t need to be named. The map() method expects  
+// a function, so we can pass one in with no name or  anything - it can be totally anonymous.
+const simplified = nums.map(function (nums) {return nums * 2});
+console.log(simplified);
 
 
-// Simplfied w/ map() + arrow function
+// above Simplified further with map() + arrow function
+const arrow = nums.map(nums => nums * 2);
+console.log(arrow);
 
 
 // With objects:
@@ -37,3 +55,6 @@ const students = [
     skill: 'CSS'
   },
 ];
+
+const studentsWithIds = students.map(student => [student.name, student.id]);
+console.log(studentsWithIds);
